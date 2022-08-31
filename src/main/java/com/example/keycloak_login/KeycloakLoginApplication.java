b317666ca50ebba503f411c3e904347eb04d43a0
noahKim <noah.kim@osckorea.com>
2022-08-31 16:29:01 +0900

    Adding React_Springboot Sample

---
+++ src/main/java/com/example/keycloak_login/KeycloakLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycloak_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakLoginApplication.class, args);
+    }
+
+}
